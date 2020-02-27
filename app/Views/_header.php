80f9888c2188365f448da6c92e4b142b0cb19648
Conrad Sollitt <57777521+ConradSollitt@users.noreply.github.com>
2020-02-27 11:02:16 -0800

    :globe_with_meridians: RTL Lang Improvements

--- app/Views/_header.php
+++ app/Views/_header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?= $app->lang ?>" dir="auto">
+<html lang="<?= $app->lang ?>" dir="<?= ($app->lang === 'ar' ? 'rtl' : 'auto'); ?>">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
