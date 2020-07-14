37392d0cd24baeeccf7404746cb7c597d6eaebd9
Conrad Sollitt <57777521+ConradSollitt@users.noreply.github.com>
2020-07-14 11:21:31 -0700

    ✨ Development updates for Laravel Valet

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 // public root directory on a web server.
 
 // For the Mac-only development environment Laravel Valet, the public index
-// file will be included rather than redirected too. For more on this topic
+// file will be included rather than redirected to. For more on this topic
 // see: https://github.com/fastsitephp/starter-site/issues/4
 
 // To run from a command line or terminal program you can use the following:
@@ -14,13 +14,6 @@
 //
 // Then open your web browser to:
 //     http://localhost:3000/
-
-// TODO - remove the following after more testing. This is related to issue:
-//  https://github.com/fastsitephp/starter-site/issues/4
-//
-// header('Content-Type: text/plain');
-// var_dump($_SERVER);
-// exit();
 
 if (isset($_SERVER['DOCUMENT_URI']) && strpos($_SERVER['DOCUMENT_URI'], '/laravel/valet/server.php') !== false) {
     require __DIR__ . '/public/index.php';
